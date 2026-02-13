fe9a89b2cb6d8b388190bfe0f71a61ca0eed5c0b
LeeMinwoo115 <dlalsdn115@gmail.com>
2026-02-13 18:43:37 +0900

    init: SyncNote init

---
+++ src/main/java/com/syncnote/domain/home/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.syncnote.domain.home.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+@Tag(name = "API Docs Home", description = "API 문서")
+public class HomeController {
+
+    @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
+    public String home() throws UnknownHostException {
+        InetAddress localhost = InetAddress.getLocalHost();
+
+        return """
+			<h1>SyncNote API Docs</h1>
+			<p>Server IP Address: %s</p>
+			<p>Server Host Name: %s</p>
+			<div>
+				<a href="swagger-ui/index.html">API 문서로 이동</a>
+			</div>
+			""".formatted(localhost.getHostAddress(), localhost.getHostName());
+    }
+}
---
+++ src/main/java/com/syncnote/global/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.syncnote.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.GET, "/").permitAll()
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+
+}
