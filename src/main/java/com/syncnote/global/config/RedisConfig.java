d8a64c25741157af13e8ba04d5a83bbf387c4173
LeeMinwoo115 <dlalsdn115@gmail.com>
2026-02-26 13:17:15 +0900

    chore: redis config 수정

--- src/main/java/com/syncnote/global/config/RedisConfig.java
+++ src/main/java/com/syncnote/global/config/RedisConfig.java
@@ -7,10 +7,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.net.URI;
 
 @Configuration
 public class RedisConfig {
@@ -23,8 +26,39 @@ public class RedisConfig {
     @Value("${spring.data.redis.password:}")
     private String password;
 
+    @Value("${spring.data.redis.url:}")
+    private String redisUrl;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
+        if (redisUrl != null && !redisUrl.isBlank()) {
+            URI uri = URI.create(redisUrl);
+
+            String uriHost = uri.getHost();
+            int uriPort = (uri.getPort() == -1) ? 6379 : uri.getPort();
+
+            RedisStandaloneConfiguration config =
+                    new RedisStandaloneConfiguration(uriHost, uriPort);
+
+            String userInfo = uri.getUserInfo();
+
+            if (userInfo != null && !userInfo.isBlank()) {
+                String password = userInfo.contains(":")
+                        ? userInfo.substring(userInfo.indexOf(':') + 1)
+                        : userInfo;
+
+                if (!password.isBlank()) {
+                    config.setPassword(RedisPassword.of(password));
+                }
+            }
+
+            LettuceClientConfiguration clientConfiguration = LettuceClientConfiguration.builder()
+                    .useSsl()
+                    .build();
+
+            return new LettuceConnectionFactory(config, clientConfiguration);
+        }
+
         RedisStandaloneConfiguration config =
                 new RedisStandaloneConfiguration(host, port);
 
